33360e056359f860061c6c88a384f9116b473e12
Anton Agafonov <tkmtldn@gmail.com>
2022-12-06 00:37:42 +0400

    update day 02

---
+++ day02/day02.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	data := GetStrings("./input02.txt")
+
+	fmt.Println(data)
+	//fmt.Printf("First answer: : %v \n", maxCalories)
+	//fmt.Printf("Second answer: : %v \n", topThree)
+}
