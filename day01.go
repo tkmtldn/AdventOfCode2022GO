e69d7752c06e36bcf02a82ef5a0aad8c444a23f7
Anton Agafonov <tkmtldn@gmail.com>
2022-12-05 22:34:48 +0400

    update day 01

--- day01.go
+++ day01.go
@@ -1,5 +1,66 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+)
 
+func GetStrings(path string) []string {
+	file, err := os.Open(path)
+	check(err)
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+
+	scan := bufio.NewScanner(file)
+
+	var result []string
+
+	for scan.Scan() {
+		result = append(result, scan.Text())
+	}
+
+	check(scan.Err())
+
+	return result
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	data := GetStrings("./input01.txt")
+	maxCalories := 0
+	currentCalories := 0
+	allCals := []int{}
+	for _, v := range data {
+		v_int, err := strconv.Atoi(v)
+		if err != nil {
+			if currentCalories > maxCalories {
+				maxCalories = currentCalories
+			}
+			allCals = append(allCals, currentCalories)
+			currentCalories = 0
+		} else {
+			currentCalories += v_int
+		}
+	}
+
+	//summing up the last elf
+	allCals = append(allCals, currentCalories)
+	if currentCalories > maxCalories {
+		maxCalories = currentCalories
+	}
+
+	fmt.Printf("First answer: : %v \n", maxCalories)
+
+	sort.Ints(allCals)
+	topThree := allCals[len(allCals)-1] + allCals[len(allCals)-2] + allCals[len(allCals)-3]
+	fmt.Printf("Second answer: : %v \n", topThree)
 }
