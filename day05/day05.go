bea3c8318c71e5faae89d3c1f7429d842549a0ce
Anton Agafonov <tkmtldn@gmail.com>
2022-12-20 20:42:16 +0400

    New files and new days added

--- day05/day05.go
+++ day05/day05.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
-func GetStrings(path string) []string {
+func GetData(path string) ([]string, [][]string) {
 	file, err := os.Open(path)
 	check(err)
 	defer func(file *os.File) {
@@ -15,15 +17,41 @@ func GetStrings(path string) []string {
 
 	scan := bufio.NewScanner(file)
 
-	var result []string
+	var result2 [][]string
+	result3 := make([]string, 3)
+	isProcedure := false
 
 	for scan.Scan() {
-		result = append(result, scan.Text())
+		if scan.Text() == "" {
+			isProcedure = true
+		} else if isProcedure {
+			str := strings.Replace(scan.Text(), "move", "", -1)
+			str = strings.Replace(str, "from", "", -1)
+			str = strings.Replace(str, "to", "", -1)
+			d := strings.Split(str, " ")
+			result2 = append(result2, d)
+		} else {
+			line := scan.Text()
+			//fmt.Println(line)
+			//if line[len(line)-1] == 93 {
+			newLine := strings.Replace(line, "   ", "[_]", -1)
+			newLine = strings.Replace(newLine, "[", "", -1)
+			newLine = strings.Replace(newLine, "]", "", -1)
+			newLine = strings.Replace(newLine, " ", "", -1)
+			fmt.Printf(line, "-", newLine)
+			for k, v := range newLine {
+				if v != 95 {
+					result3[k] += string(v)
+				}
+				//}
+			}
+		}
+
 	}
 
 	check(scan.Err())
 
-	return result
+	return result3, result2
 }
 
 func check(e error) {
@@ -32,8 +60,44 @@ func check(e error) {
 	}
 }
 
+func reverse(s string) string {
+	rns := []rune(s)
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+
+	return string(rns)
+}
+
+func beginProcs(s []string, p [][]string) string {
+	for _, v := range p {
+		move, _ := strconv.Atoi(v[1])
+		from, _ := strconv.Atoi(v[3])
+		from -= 1
+		to, _ := strconv.Atoi(v[5])
+		to -= 1
+
+		cut := reverse(s[from][:move])
+		uncut := s[from][move:]
+
+		s[to] = cut + s[to]
+		s[from] = uncut
+
+	}
+
+	ans := ""
+	fmt.Println(len(s))
+	for _, v := range s {
+		ans += string(v[0])
+	}
+
+	return ans
+}
+
 func main() {
-	data := GetStrings("./day05/input05.txt")
-	fmt.Printf("First answer: %v \n", data)
-	fmt.Printf("Second answer: %v \n", data)
+	stacks, procs := GetData("./day05/input_ex.txt")
+
+	fmt.Printf("First answer: %v \n", beginProcs(stacks, procs))
+	//fmt.Printf("Second answer: %v \n", data)
 }
--- day06/day06.go
+++ day06/day06.go
@@ -1,1 +1,63 @@
-package day06
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func GetString(path string) string {
+	file, err := os.Open(path)
+	check(err)
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+
+	scan := bufio.NewScanner(file)
+	scan.Scan()
+	result := scan.Text()
+
+	check(scan.Err())
+
+	return result
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func packetScan(s string) int {
+	for i := 0; i < len(s)-5; i++ {
+		line := s[i : i+4]
+		if (line[0] != line[1]) && (line[0] != line[2]) && (line[0] != line[3]) && (line[1] != line[2]) && (line[1] != line[3]) && (line[2] != line[3]) {
+			return i + 4
+		}
+	}
+	return 0
+}
+
+func messageScan(s string) int {
+	for i := 0; i < len(s)-15; i++ {
+		line := s[i : i+14]
+
+		total := 0
+		for j := 0; j < 14; j++ {
+			total += strings.Count(line, string(line[j]))
+		}
+		if total == 14 {
+			return i + 14
+		}
+
+	}
+	return 0
+}
+
+func main() {
+	data := GetString("./day06/input06.txt")
+
+	fmt.Printf("First answer: %v \n", packetScan(data))
+	fmt.Printf("Second answer: %v \n", messageScan(data))
+}
